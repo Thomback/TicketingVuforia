9a4f0ba70f640635b8bec96896686ed500b365cb
Thomas L <thomaslomb13@gmail.com>
2020-02-13 17:43:31 +0100

    Sensation V2
    Version probablement finale de Sensation

---
+++ Ticketing Sensation/Assets/Scripts/ControlFadeOut.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlFadeOut : MonoBehaviour
+{
+    public Animator animButton;
+    public Animator animEcran;
+
+    public float timeBeforeAnim = 7;
+
+    private void Update()
+    {
+        timeBeforeAnim -= Time.deltaTime;
+
+        if (timeBeforeAnim <= 0)
+        {
+            byeByeScreen();
+        }
+    }
+
+    public void byeByeScreen()
+    {
+        animButton.Play("ButtonByeBye");
+        animEcran.Play("EcranByeBye");
+    }
+}
